b28615829a903df9d2d0c79383b71a9a4cfe5049
Ramon Fernandes <ramonfernandes9@gmail.com>
2021-01-06 15:45:45 -0300

    Criando classe curso, adicionando dois cursos e listando na tabela e inicio da criação da classe star

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,10 +1,39 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
 
 @Component({
     selector: 'app-course-list',
     templateUrl: './course-list.component.html'
 })
-export class CourseListComponent{
+export class CourseListComponent implements OnInit{
 
+    courses: Course[] = [];
+
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name: 'Curso Angular 8',
+                imageUrl: '/assets/images/forms.png',
+                preco: 290,
+                code: 'RVF-9876',
+                duration: 120,
+                rating: 2.4,
+                realeseDate: '23/23/2021'
+            },
+            {
+                id: 2,
+                name: 'Curso java',
+                imageUrl: '/assets/images/cli.png',
+                preco: 29,
+                code: 'RVF-76',
+                duration: 150,
+                rating: 2.8,
+                realeseDate: '23/23/2021'
+            }
+        ]
+    }
+
+ 
     
 }
---
+++ src/app/courses/course.ts
@@ -0,0 +1,10 @@
+export class Course{
+    id: number;
+    name: string;
+    imageUrl: string;
+    preco: number;
+    code: string;
+    duration: number;
+    rating: number;
+    realeseDate: string;
+}
