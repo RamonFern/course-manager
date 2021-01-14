12564a5cf55b58226832c39fb0daa95d77a90142
Ramon Fernandes <ramonfernandes9@gmail.com>
2021-01-13 22:25:26 -0300

    Adicionando a pasta de arro dentro da pasta core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { Error404Component } from './error-404/error-404.component';
+import { Error404Component } from './core/component/error-404/error-404.component';
 import { CourseModule } from './courses/course.module';
 import { CoreModule } from './core/core.module';
 
@@ -22,11 +22,7 @@ import { CoreModule } from './core/core.module';
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
-        path: '**', component: Error404Component
       }
-
     ])
 
   ],
